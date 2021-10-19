78ac20551ef7437a2ff523e54df6b48999a5f411
Michael Jordan <michdan25@gmail.com>
2021-10-20 09:43:52 +1300

    commenting

--- Gallant/Assets/Scripts/AI/Actors/Actor_Tracker.cs
+++ Gallant/Assets/Scripts/AI/Actors/Actor_Tracker.cs
@@ -4,40 +4,60 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+/****************
+ * Actor_Tracker : An actor component used to track and display the damage dealt to this actor.
+ * @author : Michael Jordan
+ * @file : Actor_Tracker.cs
+ * @year : 2021
+ */
 public class Actor_Tracker : MonoBehaviour
 {
+    [Header("Settings")]
     public bool m_enableAutoHealing = false;
     public float m_peakRefreshTime = 5f;
 
+    //Where to display the text;
     private TextMeshPro m_display;
-    private float m_resistanceStat = 0;
-    private float m_damageStat = 0;
-    private DamageRecord m_peakDPS;
-    private DamageRecord m_peakHit;
-    private float m_lastHitStat = 0;
 
+    /****************
+     * DamageRecord : A struct which records the system time when a damage was received.
+     * @author : Michael Jordan
+     */
     public struct DamageRecord
     {
         public DamageRecord(float dmg) { m_amount = dmg; m_whenHit = DateTime.Now; }
         
         public float m_amount;
         private DateTime m_whenHit;
-        public bool HasElapsed(float seconds) { return (DateTime.Now - m_whenHit).TotalSeconds > seconds; }
+
+        /*******************
+         * HasElapsed : Checks the total seconds elapsed since the creation of this DamageRecord.
+         * @author : Michael Jordan
+         * @param : (float) Seconds to compare with.
+         * @return : (bool) true if the total seconds has elapsed the provided value.
+         */
+        public bool HasElapsed(float seconds) { return (DateTime.Now - m_whenHit).TotalSeconds >= seconds; }
+        
+        /*********************
+         * Refresh : Resets time when this DamageRecord was created.
+         * @author: Michael Jordan
+         */
         public void Refresh() { m_whenHit = DateTime.Now; }
     }
 
+    //Stats to record:
+    private float m_resistanceStat = 0;
+    private float m_damageStat = 0;
+    private DamageRecord m_peakDPS;
+    private DamageRecord m_peakHit;
+    private float m_lastHitStat = 0;
     public Queue<DamageRecord> m_damageRecords = new Queue<DamageRecord>();
 
+    //Called upon creation of this class
     private void Awake()
     {
         m_display = GetComponent<TextMeshPro>();
         m_peakDPS = new DamageRecord(0);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
@@ -72,9 +92,15 @@ public class Actor_Tracker : MonoBehaviour
         display += $"Last Hit: {m_lastHitStat} ({m_peakHit.m_amount})\n";
         display += $"DPS: {m_damageStat}/s ({m_peakDPS.m_amount}/s) \n";
 
-        m_display.text = display;
+        if(m_display != null)
+            m_display.text = display;
     }
 
+    /*******************
+     * RecordDamage : Records a damage value onto this tracker.
+     * @author : Michael Jordan
+     * @param : (float) damage to record.
+     */
     public void RecordDamage(float damage)
     {
         m_lastHitStat = damage;
@@ -87,6 +113,11 @@ public class Actor_Tracker : MonoBehaviour
         m_damageRecords.Enqueue(new DamageRecord(damage));
     }
 
+    /*******************
+     * RecordResistance : Records the resistance value to display onto this tracker.
+     * @author : Michael Jordan
+     * @param : (float) resistance to record.
+     */
     public void RecordResistance(float resistance)
     {
         m_resistanceStat = resistance;
