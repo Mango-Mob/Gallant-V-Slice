836175d1dcab60d85a72072746cb28a479a40e94
Michael Jordan <michdan25@gmail.com>
2021-10-29 17:33:06 +1300

    weapon data given coments

--- Gallant/Assets/Scripts/Rewards/WeaponReward.cs
+++ Gallant/Assets/Scripts/Rewards/WeaponReward.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,7 +13,7 @@ public class WeaponReward : Reward
 
     public Text m_levelText;
 
-    public GameObject m_starLoc;
+    public GameObject[] m_stars;
 
     public Text m_leftDamage;
     public Text m_leftSpeed;
@@ -26,16 +27,62 @@ public class WeaponReward : Reward
 
     public void LoadWeapon(WeaponData data, Player_Attack playerData)
     {
+        m_title.text = GetPrefix(data.abilityData.starPowerLevel) + " " + data.name + " of "+ data.abilityData.weaponTitle;
+        m_levelText.text = "Level: " + data.m_level;
+
         m_weaponImageLoc.sprite = data.weaponIcon;
         m_abilityImageLoc.sprite = data.abilityData.abilityIcon;
 
-        int damageDiff_L = data.m_damage - playerData.m_leftWeapon.m_damage;
-        float speedDiff_L = data.m_speed - playerData.m_leftWeapon.m_speed;
+        UpdateCompareField(m_leftDamage, data.m_damage - playerData.m_leftWeapon.m_damage, (float)m_damageDiffScale);
+        UpdateCompareField(m_leftSpeed, data.m_speed - playerData.m_leftWeapon.m_speed, m_speedDiffScale);
 
-        int damageDiff_R = data.m_damage - playerData.m_rightWeapon.m_damage;
-        float speedDiff_R = data.m_speed - playerData.m_rightWeapon.m_speed;
+        UpdateCompareField(m_rightDamage, data.m_damage - playerData.m_rightWeapon.m_damage, (float)m_damageDiffScale);
+        UpdateCompareField(m_rightSpeed, data.m_speed - playerData.m_rightWeapon.m_speed, m_speedDiffScale);
+    }
 
+    /*******************
+    * UpdateCompareField : Updates a text field with the aproximate damage difference ('+' equates to 0f-1f full diff scale).
+    * @author : Michael Jordan
+    * @param : (Text) The field to insert the aproximate difference (in +, - or =)
+    * @param : (float) The actual difference between the player and this weapon.
+    * @param : (float) The current difference scale.
+    */
+    private void UpdateCompareField(Text compareField, float currentDiff, float diffScale)
+    {
+        if (diffScale == 0)
+            return;
 
+        int diff = Mathf.Clamp(Mathf.CeilToInt(currentDiff / diffScale), -3, 3);
+
+        if(diff > 0)
+        {
+            compareField.text = new String('+', diff);
+            compareField.color = Color.green;
+        }
+        else if(diff < 0)
+        {
+            compareField.text = new String('-', diff);
+            compareField.color = Color.red;
+        }
+        else
+        {
+            compareField.text = "=";
+            compareField.color = Color.yellow;
+        }
+    }
+
+    private string GetPrefix(int abilityLevel)
+    {
+        switch (abilityLevel)
+        {
+            case 1:
+                return "Lesser";
+            default:
+            case 2:
+                return "";
+            case 3:
+                return "Greater";
+        }
     }
 
     public override void GiveReward()
