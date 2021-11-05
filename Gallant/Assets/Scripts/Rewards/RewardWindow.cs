946834434b6c3672d4531e90de3ff3876a6b9798
Michael Jordan <michdan25@gmail.com>
2021-11-05 20:05:21 +1300

    reward window

--- Gallant/Assets/Scripts/Rewards/RewardWindow.cs
+++ Gallant/Assets/Scripts/Rewards/RewardWindow.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 public class RewardWindow : MonoBehaviour
 {
     public GameObject m_window;
-    public GameObject m_pannel;
+    public GameObject[] m_rewardSlots;
     public GameObject m_weaponRewardOption;
     public GameObject m_itemRewardOption;
 
@@ -22,6 +22,8 @@ public class RewardWindow : MonoBehaviour
     private Image m_pressDurationImage;
 
     public List<ItemData> m_items = new List<ItemData>();
+    [Range(0.0f, 1.0f)]
+    public float[] m_weaponProbability;
 
     private const float probFirstWeapon = 1.0f;
     private const float probSecondWeapon = 0.6666f;
@@ -88,60 +90,47 @@ public class RewardWindow : MonoBehaviour
         m_rewards.Clear();
         if (level >= 0)
         {
-            for (int i = ((m_pannel.transform as RectTransform).childCount) - 1; i >= 0; i--)
+            for (int i = 0; i < m_rewardSlots.Length; i++)
             {
-                Destroy((m_pannel.transform as RectTransform).GetChild(i).gameObject);
+                for (int j = ((m_rewardSlots[i].transform as RectTransform).childCount) - 1; j >= 0; j--)
+                {
+                    Destroy((m_rewardSlots[i].transform as RectTransform).GetChild(j).gameObject);
+                }
             }
-
+            
             //Generate a random selection of rewards
             int rollSize = 10000;
             float roll = Random.Range(0, rollSize);
             List<ScriptableObject> rewards = new List<ScriptableObject>();
-            //First reward
-            if(roll <= probFirstWeapon * rollSize)// < Max
+
+            for (int i = 0; i < m_rewardSlots.Length; i++)
             {
-                rewards.Add(WeaponData.GenerateWeapon(level));
+                if (roll <= m_weaponProbability[i] * rollSize)
+                {
+                    rewards.Add(WeaponData.GenerateWeapon(level));
+                }
+                else
+                {
+                    int select = Random.Range(0, m_items.Count);
+                    rewards.Add(m_items[select]);
+                }
             }
-            else
-            {
-                int select = Random.Range(0, m_items.Count);
-                rewards.Add(m_items[select]);
-            }
-            //Second reward
-            if (roll <= probSecondWeapon * rollSize)
-            {
-                rewards.Add(WeaponData.GenerateWeapon(level));
-            }
-            else
-            {
-                int select = Random.Range(0, m_items.Count);
-                rewards.Add(m_items[select]);
-            }
-            //third reward
-            if (roll <= probThirdWeapon * rollSize)
-            {
-                rewards.Add(WeaponData.GenerateWeapon(level));
-            }
-            else
-            {
-                int select = Random.Range(0, m_items.Count);
-                rewards.Add(m_items[select]);
-            }
+            //Shuffle
             rewards.Sort((a, b) => { return 1 - 2 * Random.Range(0, 2); });
 
-            foreach (var item in rewards)
+            for (int i = 0; i < m_rewardSlots.Length; i++)
             {
-                if(item.GetType() == typeof(WeaponData))
+                if (rewards[i].GetType() == typeof(WeaponData))
                 {
-                    WeaponReward wReward = GameObject.Instantiate(m_weaponRewardOption, m_pannel.transform).GetComponent<WeaponReward>();
-                    wReward.LoadWeapon(item as WeaponData, m_player);
+                    WeaponReward wReward = GameObject.Instantiate(m_weaponRewardOption, m_rewardSlots[i].transform).GetComponent<WeaponReward>();
+                    wReward.LoadWeapon(rewards[i] as WeaponData, m_player);
                     m_rewards.Add(wReward);
                     m_rewards[m_rewards.Count - 1].m_id = m_rewards.Count - 1;
                 }
                 else
                 {
-                    ItemReward iReward = GameObject.Instantiate(m_itemRewardOption, m_pannel.transform).GetComponent<ItemReward>();
-                    iReward.LoadItem(item as ItemData, m_player);
+                    ItemReward iReward = GameObject.Instantiate(m_itemRewardOption, m_rewardSlots[i].transform).GetComponent<ItemReward>();
+                    iReward.LoadItem(rewards[i] as ItemData, m_player);
                     m_rewards.Add(iReward);
                     m_rewards[m_rewards.Count - 1].m_id = m_rewards.Count - 1;
                 }
