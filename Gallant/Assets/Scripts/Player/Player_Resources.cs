ccc3d5967df40993595434ccf06f83468468e799
William <55791230+GrizzBe@users.noreply.github.com>
2021-11-23 20:59:43 +1300

    The things I have done

--- Gallant/Assets/Scripts/Player/Player_Resources.cs
+++ Gallant/Assets/Scripts/Player/Player_Resources.cs
@@ -27,6 +27,8 @@ public class Player_Resources : MonoBehaviour
     [SerializeField] private GameObject healVFXPrefab;
     public bool m_dead { get; private set; } = false;
 
+    private Animator animatorHUD;
+
     [Header("Barrier")]
     private float m_barrierDecayTimer = 0.0f;
     public float m_barrierDecayDelay = 0.5f;
@@ -38,6 +40,8 @@ public class Player_Resources : MonoBehaviour
         barrierBar = HUDManager.instance.GetElement<UI_Bar>("Barrier");
         portrait = HUDManager.instance.GetElement<UI_PortraitHP>("Portrait");
         adrenalineOrbs = HUDManager.instance.GetElement<UI_OrbResource>("Adrenaline");
+
+        animatorHUD = HUDManager.instance.GetComponent<Animator>();
     }
     // Start is called before the first frame update
     void Start()
