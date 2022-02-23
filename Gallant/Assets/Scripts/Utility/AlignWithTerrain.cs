edc2709c3121eee8593cf7a74fe76119a4a8789b
Michael Jordan <michdan25@gmail.com>
2022-02-23 17:25:51 +1300

    blocking out

---
+++ Gallant/Assets/Scripts/Utility/AlignWithTerrain.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignWithTerrain : MonoBehaviour
+{
+    RaycastHit hit;
+    Vector3 theRay;
+
+    public LayerMask terainMask;
+    public bool m_instant = false;
+
+    void FixedUpdate()
+    {
+        Align();
+    }
+
+    private void Align()
+    {
+        theRay = -transform.up;
+
+        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y, transform.position.z),
+            theRay, out hit, 20, terainMask))
+        {
+
+            Quaternion targetRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.parent.rotation;
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, (m_instant ? 1.0f : Time.deltaTime / 0.15f));
+        }
+    }
+}
---
+++ Gallant/Assets/Scripts/Utility/WorldToCanvas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldToCanvas : MonoBehaviour
+{
+    public Transform m_anchorTransform;
+    private Camera m_camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ForceUpdate();
+    }
+
+    public void ForceUpdate()
+    {
+        if (m_anchorTransform != null)
+        {
+            Vector3 pos = m_camera.WorldToScreenPoint(m_anchorTransform.transform.position);
+            (transform as RectTransform).position = pos;
+        }
+    }
+}
