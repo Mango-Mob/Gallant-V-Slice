6a46e4d45ea4f79345aa6f94c0e87fd4fdfe86ce
William <55791230+GrizzBe@users.noreply.github.com>
2022-03-02 18:42:50 +1300

    New environmental dangers added

--- Gallant/Assets/Scripts/Hazards/GroundSurface.cs
+++ Gallant/Assets/Scripts/Hazards/GroundSurface.cs
@@ -2,20 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IceGround : MonoBehaviour
+public class GroundSurface : MonoBehaviour
 {
+     public enum SurfaceType
+    {
+        ICE,
+        BOG,
+        LAVA,
+    }
+
+    public SurfaceType m_surfaceType;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Player_Movement>())
         {
-            other.GetComponent<Player_Movement>().m_onIce = true;
+            other.GetComponent<Player_Movement>().m_touchedSurfaces.Add(m_surfaceType);
         }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.GetComponent<Player_Movement>())
         {
-            other.GetComponent<Player_Movement>().m_onIce = false;
+            other.GetComponent<Player_Movement>().m_touchedSurfaces.Remove(m_surfaceType);
         }
     }
 }
