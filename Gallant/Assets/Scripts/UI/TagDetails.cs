aec2c4d0ed3c66e88cd9d34e3c5f0d10dc256988
Michael Jordan <michdan25@gmail.com>
2022-03-14 14:24:07 +1300

    tag prefabs

--- Gallant/Assets/Scripts/UI/TagDetails.cs
+++ Gallant/Assets/Scripts/UI/TagDetails.cs
@@ -6,10 +6,13 @@ using UnityEngine.UI;
 [ExecuteInEditMode]
 public class TagDetails : MonoBehaviour
 {
+    public int weight;
+
     public string m_tagTitle;
     public Color m_tagColor;
     public Color m_textColor;
     public uint m_fontSize = 20;
+
     // Update is called once per frame
     void Update()
     {
@@ -23,5 +26,13 @@ public class TagDetails : MonoBehaviour
             text.fontSize = (int)m_fontSize;
             text.color = m_textColor;
         }
+    }
+
+    public static int Compare(TagDetails a, TagDetails b)
+    {
+        if (a.weight == b.weight)
+            return 0;
+
+        return (a.weight - b.weight) / Mathf.Abs(a.weight - b.weight);
     }
 }
