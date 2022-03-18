e26d2a72f3216d5214e2f5f5cf34853b38d965d0
Michael Jordan <michdan25@gmail.com>
2022-03-18 16:57:04 +1300

    tutorial done, just need tweaks

---
+++ Gallant/Assets/Scripts/ActorSystem/Spawning/PortalSpawnData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace ActorSystem.Spawning
+{
+    public class PortalSpawnData : SpawnDataGenerator
+    {
+        protected override Task GenerateSpawnPoints()
+        {
+            m_spawnPoints = new List<SpawnData>();
+
+            for (int i = 0; i < m_sections; i++)
+            {
+                Vector2 loc;
+                if(m_isCircle)
+                {
+                    loc = Random.insideUnitCircle;
+                }
+                else
+                {
+                    loc = new Vector2(Random.Range(-m_spawnSize, m_spawnSize), Random.Range(-m_spawnSize, m_spawnSize));
+                }
+                SpawnData data = new SpawnData();
+                data.endPoint = transform.position + new Vector3(loc.x, 0, loc.y);
+                data.startPoint = data.endPoint;
+
+                NavMeshHit navHit;
+                if (NavMesh.SamplePosition(data.endPoint, out navHit, m_navSampleRadius, NavMesh.AllAreas))
+                {
+                    data.navPoint = navHit.position;
+                }
+
+                m_spawnPoints.Add(data);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public override void OnDrawGizmos()
+        {
+            base.OnDrawGizmos();
+        }
+    }
+}
--- Gallant/Assets/Scripts/TutorialManager.cs
+++ Gallant/Assets/Scripts/TutorialManager.cs
@@ -1,4 +1,5 @@
 using ActorSystem.AI;
+using ActorSystem.AI.Users;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,15 @@ public class TutorialManager : MonoBehaviour
     public Actor m_guide;
 
     public Transform[] m_tutorialPositions;
+    public TextAsset[] m_tutorialDialog;
 
+    public TextAsset[] m_classReSpecDialog;
+
+    public ClassData m_warrior;
+    public ClassData m_mage;
+    public ClassData m_hunter;
+
+    public Room m_combatSection;
     private int current = 0;
     // Start is called before the first frame update
     void Start()
@@ -17,6 +26,7 @@ public class TutorialManager : MonoBehaviour
         {
             GameManager.Instance.m_player.transform.position = transform.position;
             GameManager.Instance.m_player.transform.forward = transform.forward;
+            (m_guide as LoreKeeper).m_dialog = m_tutorialDialog[current];
         }
     }
 
@@ -32,13 +42,50 @@ public class TutorialManager : MonoBehaviour
         {
             m_guide.SetTargetOrientaion(m_guide.transform.position + m_guide.m_myBrain.m_legs.velocity.normalized);
         }
+
+        if(m_combatSection.IsComplete() && current == 3)
+        {
+            current++;
+            m_guide.SetTargetLocation(m_tutorialPositions[current].position);
+            m_guide.m_myBrain.m_myOutline.enabled = false;
+            (m_guide as LoreKeeper).m_dialog = m_tutorialDialog[current - 1];
+        }
     }
 
-    public void TutorialOne()
+    public void AdvanceTutorial()
     {
-        current++;
-        m_guide.SetTargetLocation(m_tutorialPositions[current].position);
-        m_guide.m_myBrain.m_myOutline.enabled = false;
+        if (current < 3)
+        {
+            current++;
+            m_guide.SetTargetLocation(m_tutorialPositions[current].position);
+            m_guide.m_myBrain.m_myOutline.enabled = false;
+            (m_guide as LoreKeeper).m_dialog = m_tutorialDialog[current];
+        }
+
+        if(current == 3)
+        {
+            RewardManager.Instance.Show(m_warrior.rightWeapon, m_mage.rightWeapon, m_hunter.rightWeapon, SelectClass);
+        }
+    }
+
+    public void SelectClass(int selected)
+    {
+        switch(selected)
+        {
+            default:
+            case 0:
+                GameManager.Instance.m_player.GetComponent<Player_Controller>().SelectClass(m_warrior);
+                (m_guide as LoreKeeper).m_dialog = m_classReSpecDialog[selected];
+                break;
+            case 1:
+                GameManager.Instance.m_player.GetComponent<Player_Controller>().SelectClass(m_mage);
+                (m_guide as LoreKeeper).m_dialog = m_classReSpecDialog[selected];
+                break;
+            case 2:
+                GameManager.Instance.m_player.GetComponent<Player_Controller>().SelectClass(m_hunter);
+                (m_guide as LoreKeeper).m_dialog = m_classReSpecDialog[selected];
+                break;
+        }
     }
 
     private void OnDrawGizmos()
