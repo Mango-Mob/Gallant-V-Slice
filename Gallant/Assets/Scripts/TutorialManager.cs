29bfd44abe50c2fa3eaee08a4c16986d2c0a5cb8
Michael Jordan <michdan25@gmail.com>
2022-03-18 18:18:21 +1300

    tutorial done

--- Gallant/Assets/Scripts/TutorialManager.cs
+++ Gallant/Assets/Scripts/TutorialManager.cs
@@ -14,6 +14,7 @@ public class TutorialManager : MonoBehaviour
     public TextAsset[] m_tutorialDialog;
 
     public TextAsset[] m_classReSpecDialog;
+    public TextAsset m_playerDeathDialog;
 
     public ClassData m_warrior;
     public ClassData m_mage;
@@ -22,10 +23,13 @@ public class TutorialManager : MonoBehaviour
     public Room m_combatSection;
 
     public Transform m_respawn;
+
+    public GameObject[] m_mainGameObject;
     private Image m_fade;
 
     private int current = 0;
     private bool m_isRespawning = false;
+    private bool m_playerHasDied = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +40,18 @@ public class TutorialManager : MonoBehaviour
             GameManager.Instance.m_player.transform.position = transform.position;
             GameManager.Instance.m_player.transform.forward = transform.forward;
             (m_guide as LoreKeeper).m_dialog = m_tutorialDialog[current];
+
+            foreach (var item in m_mainGameObject)
+            {
+                item.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach (var item in m_mainGameObject)
+            {
+                item.SetActive(true);
+            }
         }
     }
 
@@ -55,7 +71,7 @@ public class TutorialManager : MonoBehaviour
             m_guide.SetTargetOrientaion(m_guide.transform.position + m_guide.m_myBrain.m_legs.velocity.normalized);
         }
 
-        if(m_combatSection.IsComplete() && current == 3)
+        if(m_combatSection.IsComplete() && !GameManager.Instance.IsInCombat && current == 3)
         {
             current++;
             m_guide.SetTargetLocation(m_tutorialPositions[current].position);
@@ -66,6 +82,13 @@ public class TutorialManager : MonoBehaviour
         if(GameManager.Instance.m_player.GetComponent<Player_Controller>().playerResources.m_dead && !m_isRespawning)
         {
             StartCoroutine(RespawnPlayer());
+            m_playerHasDied = true;
+        }
+
+        if(m_playerHasDied)
+        {
+            (m_guide as LoreKeeper).m_dialog = m_playerDeathDialog;
+            m_playerHasDied = false;
         }
     }
 
@@ -82,6 +105,14 @@ public class TutorialManager : MonoBehaviour
         if(current == 3)
         {
             RewardManager.Instance.Show(m_warrior.rightWeapon, m_mage.rightWeapon, m_hunter.rightWeapon, SelectClass);
+        }
+
+        if(current == 4)
+        {
+            current++;
+            m_guide.SetTargetLocation(m_tutorialPositions[current].position);
+            m_guide.m_myBrain.m_myOutline.enabled = false;
+            (m_guide as LoreKeeper).m_dialog = m_tutorialDialog[current - 1];
         }
     }
 
@@ -126,8 +157,10 @@ public class TutorialManager : MonoBehaviour
             yield return new WaitForEndOfFrame();
             m_fade.color = new Color(0, 0, 0, timeOut / 1.0f);
         }
+        m_fade.enabled = false;
         player.GetComponent<Player_Controller>().m_isDisabledInput = false;
         m_isRespawning = false;
+        
         yield return null;
     }
 
