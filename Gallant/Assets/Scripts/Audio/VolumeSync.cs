691b2ffc06faba75659b0a8444d6b5ac1704103c
Michael Jordan <michdan25@gmail.com>
2022-03-24 16:58:25 +1300

    volume sync

---
+++ Gallant/Assets/Scripts/Audio/VolumeSync.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Audio
+{
+    public class VolumeSync : MonoBehaviour
+    {
+        public AudioManager.VolumeChannel m_myChannel;
+
+        private AudioSource m_source;
+        private void Awake()
+        {
+            m_source = GetComponent<AudioSource>();
+        }
+        public void Update()
+        {
+            m_source.volume = AudioManager.Instance.GetVolume(m_myChannel, null);
+        }
+    }
+}
