2fcc4631c9ec2cdb71b929256b71cfeba125f073
Roger Painting <apollocreed1309@windowslive.com>
2022-04-03 18:43:36 +1200

    GroundSlash

---
+++ Gallant/Assets/Scenes/Test Scenes/Roger/Misc/GroundSlashViewTestRP.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSlashViewTestRP : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject projectile;
+    public Transform firePoint;
+    public float fireRate = 4;
+
+    private Vector3 destination;
+    private float timeToFire;
+    private GroundSlashRP groundSlashScript;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= timeToFire)
+        {
+            timeToFire = Time.time + 1 / fireRate;
+            ShootProjectile();
+        }
+    }
+
+    void ShootProjectile()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+
+        destination = ray.GetPoint(1000);
+
+        InstantiateProjectile();
+
+    }
+
+    void InstantiateProjectile()
+    {
+        var projectileObj = Instantiate(projectile, firePoint.position, Quaternion.identity) as GameObject;
+
+        groundSlashScript = projectileObj.GetComponent<GroundSlashRP>();
+        RotateToDestination(projectileObj, destination, true);
+        projectile.GetComponent<Rigidbody>().velocity = transform.forward * groundSlashScript.speed;
+
+    }
+
+    void RotateToDestination(GameObject obj, Vector3 destination, bool onlyY)
+    {
+        var direction = destination - obj.transform.position;
+        var rotation = Quaternion.LookRotation(direction);
+
+        if (onlyY)
+        {
+            rotation.x = 0;
+            rotation.y = 0;
+
+        }
+
+        obj.transform.localRotation = Quaternion.Lerp(obj.transform.rotation, rotation, 1);
+
+    }
+}
