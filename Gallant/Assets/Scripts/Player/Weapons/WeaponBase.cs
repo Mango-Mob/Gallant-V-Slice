eb6e9679ec6e6b90102f722776252aa3fdc9ede8
William <55791230+GrizzBe@users.noreply.github.com>
2022-05-02 18:27:24 +1200

    Weapon trails

--- Gallant/Assets/Scripts/Player/Weapons/WeaponBase.cs
+++ Gallant/Assets/Scripts/Player/Weapons/WeaponBase.cs
@@ -8,7 +8,6 @@ public abstract class WeaponBase : MonoBehaviour
     public Player_Controller playerController { private set; get; }
     public GameObject m_weaponObject;
     public WeaponData m_weaponData;
-
     public bool m_isInUse { private set; get; } = false;
 
     public GameObject m_objectPrefab;
@@ -24,6 +23,8 @@ public abstract class WeaponBase : MonoBehaviour
     protected bool m_isVFXColored = false;
     protected bool m_isAltVFXColored = false;
 
+    public ParticleSystem[] m_weaponTrailParticles;
+
     // Start is called before the first frame update
     protected void Awake()
     {
@@ -32,11 +33,25 @@ public abstract class WeaponBase : MonoBehaviour
 
     protected void Start()
     {
+        m_weaponTrailParticles = m_weaponObject.GetComponentsInChildren<ParticleSystem>();
+        if (m_weaponData.abilityData != null)
+        {
+            foreach (var particleSystem in m_weaponTrailParticles)
+            {
+                ParticleSystem.MainModule mainModule = particleSystem.main;
+                Color newColor = m_weaponData.abilityData.droppedEnergyColor;
+                newColor.a = mainModule.startColor.color.a;
+                mainModule.startColor = new ParticleSystem.MinMaxGradient(newColor);
+            }
+        }
     }
     // Update is called once per frame
     protected void Update()
     {
-        
+        if (!playerController.animator.GetBool((m_hand == Hand.LEFT) ? "UsingLeft" : "UsingRight"))
+        {
+            SetTrailActive(false);
+        }
     }
     private void OnDestroy()
     {
@@ -73,7 +88,16 @@ public abstract class WeaponBase : MonoBehaviour
 
     public void SetHand(Hand _hand) { m_hand = _hand; }
     public void SetInUse(bool _inUse) { m_isInUse = _inUse; }
-
+    public void SetTrailActive(bool _active)
+    {
+        foreach (var particleSystem in m_weaponTrailParticles)
+        {
+            if (_active)
+                particleSystem.Play();
+            else if (!_active)
+                particleSystem.Stop();
+        }
+    }
     public virtual string GetWeaponName()
     {
         if (m_weaponData.overrideAnimation =="")
