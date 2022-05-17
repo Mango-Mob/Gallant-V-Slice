3536fe25bc13c09cd25f89264f73156cdaa72de3
William <55791230+GrizzBe@users.noreply.github.com>
2022-05-17 12:04:12 +1200

    Inkman run animation update

--- Gallant/Assets/Scripts/Player/Player_Controller.cs
+++ Gallant/Assets/Scripts/Player/Player_Controller.cs
@@ -73,7 +73,7 @@ public class Player_Controller : MonoBehaviour
     private CameraBounds m_cameraBounds;
     public bool m_cameraFreeze = false;
     private float m_walkRunLerp = 0.0f;
-    private float m_walkRunLerpSpeed = 5.0f;
+    private float m_walkRunLerpSpeed = 2.0f;
 
     private void Awake()
     {
@@ -173,11 +173,6 @@ public class Player_Controller : MonoBehaviour
         //    swordRunWeight += playerAttack.m_leftWeapon.GetWeaponName() == "Sword" ? -1.0f : 0.0f;
         //if (playerAttack.m_rightWeaponData != null)
         //    swordRunWeight += playerAttack.m_rightWeapon.GetWeaponName() == "Sword" ? 1.0f : 0.0f;
-        
-        // Walk run lerp
-        //m_walkRunLerp = Mathf.Clamp01(m_walkRunLerp + (animator.GetFloat("Vertical") > 0.5f ? 1.0f : -1.0f) * Time.deltaTime * m_walkRunLerpSpeed);
-        //animator.SetFloat("WalkRunBlend", m_walkRunLerp);
-        animator.SetFloat("WalkRunBlend", 0.5f);
 
         if ((!rightAttackHeld && !leftAttackHeld) || playerMovement.m_isRolling)
             playerAttack.ToggleBlock(false);
@@ -230,6 +225,15 @@ public class Player_Controller : MonoBehaviour
 
         // Move player
         playerMovement.Move(GetPlayerMovementVector(), GetPlayerAimVector(), InputManager.Instance.IsBindDown("Roll", gamepadID), Time.deltaTime);
+
+        // Walk run lerp
+        bool isWalkSpeed = animator.GetFloat("Vertical") <= 0.5f;
+        m_walkRunLerp = Mathf.Clamp01(m_walkRunLerp + (isWalkSpeed ? -1.0f : 1.0f) * Time.deltaTime * m_walkRunLerpSpeed);
+        if (isWalkSpeed)
+        {
+            animator.SetFloat("Vertical", animator.GetFloat("Vertical") * (1.0f + (1.0f - m_walkRunLerp)));
+        }
+        animator.SetFloat("WalkRunBlend", m_walkRunLerp);
 
         if (!playerMovement.m_isStunned && !playerMovement.m_isRolling) // Make sure player is not stunned
         {
