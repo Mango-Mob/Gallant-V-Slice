c6c6e8d8b8ce5c3a55c117311d10da1e77b954e8
Michael Jordan <michdan25@gmail.com>
2022-05-21 11:03:56 +1200

    build fixes

--- Gallant/Assets/Scripts/Navigation/NavigationPortal.cs
+++ Gallant/Assets/Scripts/Navigation/NavigationPortal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ActorSystem.AI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -50,7 +51,7 @@ public class NavigationPortal : MonoBehaviour
     public void Update()
     {
         m_portalMainObj.SetActive(!GameManager.Instance.IsInCombat && !RewardManager.Instance.IsVisible);
-        m_portalAnimator.SetBool("IsVisible", !GameManager.Instance.IsInCombat && !RewardManager.Instance.IsVisible);
+        m_portalAnimator.SetBool("IsVisible", !GameManager.Instance.IsInCombat && !RewardManager.Instance.IsVisible );
         GetComponent<Collider>().enabled = !GameManager.Instance.IsInCombat && !NavigationManager.Instance.IsVisible;
     }
 
