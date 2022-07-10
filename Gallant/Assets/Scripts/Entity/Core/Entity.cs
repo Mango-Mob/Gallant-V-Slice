bf80d6d6aba1e07aea4bebe241fa1a1cc52a8722
Michael Jordan <michdan25@gmail.com>
2022-07-11 11:05:08 +1200

    Entity Base

---
+++ Gallant/Assets/Scripts/Entity/Core/Entity.cs
@@ -0,0 +1,101 @@
+﻿using EntitySystem.Data;
+using UnityEngine;
+
+namespace EntitySystem.Core
+{
+    /// <summary>
+    /// Base entity class that is the parent to all moving gameobjects.
+    /// </summary>
+    public abstract class Entity : MonoBehaviour
+    {
+        public EntityData DataOnLoad;
+
+        [Header("Base Entity Stats")]
+        //Current Health and maximum health
+        public float HP;
+        protected float MaxHP { get { return DataOnLoad.HP; } }
+
+        //Current speed and default speed
+        public float Speed;
+        protected float DefaultSpeed { get { return DataOnLoad.Speed; } }
+
+        //Current Stamina and maximum stamina
+        public float Stamina;
+        protected float MaxStamina { get { return DataOnLoad.Stamina; } }
+
+        //Current Defence and maximum defence
+        public float Defence;
+        protected float DefaultDefence { get { return DataOnLoad.Defence; } }
+
+        //Current Ward and maximum ward
+        public float Ward;
+        protected float DefaultWard { get { return DataOnLoad.Ward; } }
+
+        public bool IsDead { get { return HP <= 0; } }
+
+        public enum DamageType
+        {
+            Physical,
+            Ability,
+            True
+        }
+
+        protected virtual void Awake()
+        {
+            if(DataOnLoad)
+            {
+                RefreshEntity();
+            }
+            else
+            {
+                Debug.LogError($"Entity {this.name} was loaded without data. Entity was destroyed.");
+                Destroy(this);
+            }
+        }
+
+        /// <summary>
+        /// Damages the entity directly, and calculates the damage dealt depending on the penitration and type of damage.
+        /// </summary>
+        /// <param name="_damage">Total damage dealt to the entity</param>
+        /// <param name="_pen">Total penitration of the attack</param>
+        /// <param name="_type">Type of damage of the attack</param>
+        /// <param name="_source">Source of the damage</param>
+        /// <param name="_playHurtSound">Weither the attack should play hurt sounds.</param>
+        /// <returns>Status of the entity, if it has been killed by this attack.</returns>
+        protected abstract bool DealDamageToEntity(float _damage, float _pen, Entity.DamageType _type, GameObject _source = null, bool _playHurtSound = false);
+
+        /// <summary>
+        /// Reset's this entity back to spawning stats
+        /// </summary>
+        protected void RefreshEntity()
+        {
+            this.HP = this.MaxHP;
+            this.Speed = this.DefaultSpeed;
+            this.Stamina = this.MaxStamina;
+            this.Defence = this.DefaultDefence;
+            this.Ward = this.DefaultWard;
+        }
+
+        /// <summary>
+        /// Calculates the modifier to be multiplied to the damage.
+        /// </summary>
+        /// <param name="_type">Type of the attack</param>
+        /// <param name="_resist">Amount of this entites' resistance</param>
+        /// <param name="_atkPen">Total Penitration of the attack</param>
+        /// <returns>The value to multiply the damage with.</returns>
+        public static float CalculateDamageNegated(DamageType _type, float _resist, float _atkPen)
+        {
+            _resist *= (100f / (100f + _atkPen));
+
+            switch (_type)
+            {
+                case DamageType.Physical:
+                case DamageType.Ability:
+                    return (100f / (100f + _resist));
+                default:
+                case DamageType.True:
+                    return 1f;
+            }
+        }
+    }
+}
---
+++ Gallant/Assets/Scripts/Entity/Data/EntityData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace EntitySystem.Data
+{
+    public class EntityData : ScriptableObject
+    {
+        [Header("Base Entity Stats")]
+        public float Name;
+        public float HP;
+        public float Speed;
+        public float Stamina;
+        public float Defence;
+        public float Ward;
+    }
+}
